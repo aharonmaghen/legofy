61c748a56cb798454cf917a2288b1b7aa64ce5b1
aharonmaghen <aharonmaghen@gmail.com>
2021-08-31 22:41:26 +0300

    Add functionality to split image

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,15 @@
 import sys
-from image import parse_image
+from PIL import Image
 
-image = parse_image(sys.argv)
+image = Image.open('images/image3.jpg')
 
-print(image)
+slices = []
 
-image.show()
+slice_height = int(image.height/10)
+slice_width = int(image.width/10)
+
+for x in range(0, 10):
+	for y in range(0, 10):
+		slices.append(image.crop((y*slice_width, x*slice_height, y*slice_width + slice_width, x*slice_height + slice_height)))
+
+print(slice_height, slice_width)
