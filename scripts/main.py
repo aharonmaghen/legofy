d343ee4a23ddb162c4d680300ac3cbf00ee67257
aharonmaghen <aharonmaghen@gmail.com>
2021-09-01 01:55:43 +0300

    Add functionality to split image into 100 chunks and produce \'legofied\' version

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,37 @@
-import sys
 from PIL import Image
+import numpy as np
 
 image = Image.open('images/image3.jpg')
+chunks = np.empty([10, 10], dtype=object)
 
-slices = []
-
-slice_height = int(image.height/10)
-slice_width = int(image.width/10)
+chunk_height = int(image.height/10)
+chunk_width = int(image.width/10)
 
 for x in range(0, 10):
 	for y in range(0, 10):
-		slices.append(image.crop((y*slice_width, x*slice_height, y*slice_width + slice_width, x*slice_height + slice_height)))
+		chunks[x, y] = image.crop((y*chunk_width, x*chunk_height, y*chunk_width + chunk_width, x*chunk_height + chunk_height))
 
-print(slice_height, slice_width)
+chunk_averages = np.empty([10, 10], dtype=object)
+
+for row in range(0, chunks.shape[0]):
+	for col in range(0, chunks.shape[1]):
+		curr_chunk = chunks[row, col]
+		r, g, b = 0, 0, 0
+		for x in range(curr_chunk.width):
+			for y in range(curr_chunk.height):
+				r += curr_chunk.getpixel((x, y))[0]
+				g += curr_chunk.getpixel((x, y))[1]
+				b += curr_chunk.getpixel((x, y))[2]
+		pixel_count = curr_chunk.width*curr_chunk.height
+		r = int(r/pixel_count)
+		g = int(g/pixel_count)
+		b = int(b/pixel_count)
+		chunk_averages[row, col] = (r, g, b)
+
+legofied_image = Image.new('RGB', (10, 10))
+
+for row in range(0, chunk_averages.shape[0]):
+	for col in range(0, chunk_averages.shape[1]):
+		legofied_image.putpixel((row, col), chunk_averages[row, col])
+
+legofied_image.show()
