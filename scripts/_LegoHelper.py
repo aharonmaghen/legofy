ad181e2204a5ba066fd84625490d1f9d75358870
aharonmaghen <aharonmaghen@gmail.com>
2021-09-11 23:23:00 +0300

    Rid print statement

--- scripts/_LegoHelper.py
+++ scripts/_LegoHelper.py
@@ -34,7 +34,6 @@ class LegoColors:
 
 		r1, g1, b1 = rgb[0], rgb[1], rgb[2]
 		for color in lego_colors.items():
-			print(color)
 			r2, g2, b2 = color[1][0], color[1][1], color[1][2]
 			distances.append((sqrt((r2-r1)**2 + (g2-g1)**2 + (b2-b1)**2), color[0]))
 		
